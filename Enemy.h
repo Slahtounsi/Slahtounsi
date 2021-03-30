2ebe94798a4b6cc90cbd74f21f207f1ad5d83944
Slahtounsi <81621142+Slahtounsi@users.noreply.github.com>
2021-03-30 11:07:54 +0100

    Create Enemy.h

---
+++ Enemy.h
@@ -0,0 +1,32 @@
+
+#ifndef monster_h
+#define monster_h
+#define ENNEMY_WIDTH 80
+#define ENNEMY_HEIGHT 80
+#define TIME_BETWEEN_2_FRAMES 6
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_mixer.h>
+#include <SDL/SDL_ttf.h>
+#include <time.h>
+#include "test.h"
+
+typedef struct ENNEMY
+{
+    
+    SDL_Surface *sprite;
+    SDL_Rect pos;
+    int frameNumber, frameTimer;
+    int direction ;
+    int limite;
+    
+} ENNEMY;
+void initializeEnnemy();
+void drawEnnemy(stage s);
+void drawanimatedEnnemy(personnage personnage,stage s);
+ENNEMY monstre;
+
+
+
+#endif /* monster_h */
