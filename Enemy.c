e3ba7d0a5890086681c0df3615a93575b0e9c292
Slahtounsi <81621142+Slahtounsi@users.noreply.github.com>
2021-03-30 11:16:47 +0100

    Update Enemy.c

--- Enemy.c
+++ Enemy.c
@@ -3,9 +3,8 @@
 #include <SDL/SDL_image.h>
 #include <SDL/SDL_mixer.h>
 #include <SDL/SDL_ttf.h>
-#include <time.h>
 #include "monster.h"
-#include "test.h"
+
 
 void initializeEnnemy(ENNEMY monstre)
 {
